1469cdcff1c65bbaa3b8a113183a4ac286c5bf76
Eren GUN <erengun4@gmail.com>
2023-08-23 23:30:34 +0300

    Implement endpoints

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,6 +1,7 @@
-class Enpoints {
-  Enpoints._();
-  static const String baseUrl = '';
+class Endpoints {
+  Endpoints._();
+  static const String baseUrl = 'https://api.jointact.com/App';
+  static const String devKey = 'F90322B4-82D9-4B6F-A675-192DEFA765AB';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 30000;
 
