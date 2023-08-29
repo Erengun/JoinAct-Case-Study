20db784ba81383f498a4f432fcee384548277679
Eren GUN <erengun4@gmail.com>
2023-08-29 19:02:17 +0300

    Improve ui of NoUser page

--- lib/ui/features/user/no_user.dart
+++ lib/ui/features/user/no_user.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../config/router/app_router.dart';
+import '../../widgets/app_bar_gone.dart';
 
 class NoUser extends StatelessWidget {
   const NoUser({super.key});
@@ -10,8 +11,14 @@ class NoUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const EmptyAppBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/3d%20icons%2Fnotebook-dynamic-color.png?alt=media&token=c644a0d1-4f53-42a8-a34a-b7853c256903',
+            fit: BoxFit.cover,
+          ),
           const Center(
             child: Text('No user found'),
           ),
@@ -26,3 +33,4 @@ class NoUser extends StatelessWidget {
     );
   }
 }
+
