ca6bc36a48537cba2ffc4b01c3305b1de5841cb2
Eren GUN <erengun4@gmail.com>
2023-08-29 13:52:47 +0300

    Implement a context extension to handle sncakbar

--- lib/utils/context_extensions.dart
+++ lib/utils/context_extensions.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 
 /// Extension for BuildContext to get theme, textTheme and colorScheme
@@ -5,4 +6,52 @@ extension BuildContextExtensions on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
+}
+
+extension ContextExtension on BuildContext {
+  void showAwesomeSnackBar({
+    required String title,
+    required String message,
+    String actionText = 'OK',
+    void Function()? onActionPressed,
+  }) {
+    final SnackBar snackBar = SnackBar(
+      elevation: 0,
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        title: title,
+        message: message,
+        contentType: ContentType.failure,
+      ),
+    );
+
+    ScaffoldMessenger.of(this)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+
+  void showAwesomeMaterialBanner({
+    required String title,
+    required String message,
+    String actionText = 'OK',
+    void Function()? onActionPressed,
+  }) {
+    final MaterialBanner materialBanner = MaterialBanner(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      forceActionsBelow: true,
+      content: AwesomeSnackbarContent(
+        title: title,
+        message: message,
+        contentType: ContentType.success,
+        inMaterialBanner: true,
+      ),
+      actions: const <Widget>[SizedBox.shrink()],
+    );
+
+    ScaffoldMessenger.of(this)
+      ..hideCurrentMaterialBanner()
+      ..showMaterialBanner(materialBanner);
+  }
 }
